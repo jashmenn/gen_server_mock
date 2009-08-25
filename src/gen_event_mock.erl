242437b4526da07fb6e9e1297b05087fbe509300
Micah Warren <micahw@devmicah.fusedsolutions.com>
2009-08-25 18:08:33 -0400

    gen_event_mock first attempt.

---
+++ src/gen_event_mock.erl
@@ -0,0 +1,259 @@
+%%%-------------------------------------------------------------------
+%%% File          : gen_event_mock.erl
+%%% Author        : Micah Warren
+%%% Organization  : SpiceCSM
+%%% Project       : gen_server_mock
+%%% Description   : 
+%%%
+%%% Created       :  8/25/09
+%%%-------------------------------------------------------------------
+-module(gen_event_mock).
+-behaviour(gen_event).
+
+% API
+-export([
+	start/0,
+	start/1,
+	supplant/2,
+	stop/1,
+	expect/3,
+	expect_call/2,
+	expect_event/2,
+	expect_info/2,
+	assert_expectations/1
+]).
+
+-export([
+	init/1,
+	handle_event/2,
+	handle_call/2,
+	handle_info/2,
+	terminate/2,
+	code_change/3
+]).
+
+-record(state, {
+        expectations,
+		unexpected = []
+}).
+
+-record(expectation, {
+        type,
+        lambda
+}).
+
+% steal assert from eunit
+-define(assert(BoolExpr),
+    ((fun () ->
+        case (BoolExpr) of
+        true -> ok;
+        __V -> .erlang:error({assertion_failed,
+                      [{module, ?MODULE},
+                       {line, ?LINE},
+                       {expression, (??BoolExpr)},
+                       {expected, true},
+                       {value, case __V of false -> __V;
+                           _ -> {not_a_boolean,__V}
+                           end}]})
+        end
+      end)())).
+
+-define(raise(ErrorName),
+    erlang:error({ErrorName,
+                  [{module, ?MODULE},
+                      {line, ?LINE}]})).
+
+-define(raise_info(ErrorName, Info),
+    erlang:error({ErrorName,
+                  [{module, ?MODULE},
+                      {line, ?LINE},
+                      {info, Info}
+                  ]})).
+
+
+-define (DEBUG, true).
+-define (TRACE(X, M), case ?DEBUG of
+  true -> io:format(user, "TRACE ~p:~p ~p ~p~n", [?MODULE, ?LINE, X, M]);
+  false -> ok
+end).
+
+%%====================================================================
+%% API
+%%====================================================================
+
+
+start() ->
+	{ok, Pid} = gen_event:start_link(),
+	gen_event:add_handler(Pid, ?MODULE, []),
+	{ok, Pid}.
+
+start(Nom) ->
+	{ok, Pid} = gen_event:start_link({local, Nom}),
+	gen_event:add_handler(Pid, ?MODULE, []),
+	{ok, Pid}.
+
+%% @doc See gen_event:swap_Handler
+supplant(EventRef, OldHandler) ->
+	gen_event:swap_handler(EventRef, OldHandler, {?MODULE, []}).
+	
+expect(Mock, Type, Callback) ->
+    Exp = #expectation{type=Type, lambda=Callback},
+    added = gen_event:call(Mock, ?MODULE, {expect, Exp}),
+    ok.
+
+expect_call(Mock, Callback) ->
+    expect(Mock, call, Callback).
+
+expect_event(Mock, Callback) ->
+	expect(Mock, event, Callback).
+
+expect_info(Mock, Callback) ->
+	expect(Mock, info, Callback).
+
+assert_expectations(Mock) when is_pid(Mock) ->
+    assert_expectations([Mock]);
+assert_expectations([H|T]) ->
+    case gen_event:call(H, ?MODULE, assert_expectations) of
+		ok ->
+			assert_expectations(T);
+		{Errname, Info} ->
+			?raise_info(Errname, Info),
+			ok
+	end;
+assert_expectations([]) ->
+    ok.
+
+stop(H) when is_pid(H) ->
+    stop([H]);
+stop([H|T]) ->
+    gen_event:stop(H),
+    stop(T);
+stop([]) ->
+    ok.
+
+%%====================================================================
+%% gen_event callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%--------------------------------------------------------------------
+
+init(_Args) -> 
+    InitialState = #state{expectations=[]},
+    {ok, InitialState}.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_event
+%%--------------------------------------------------------------------
+
+handle_event(Msg, State) ->
+    {ok, _Reply, NewState} = reply_with_next_expectation(event, Msg, State),
+    {ok, NewState}.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%--------------------------------------------------------------------
+
+% return the state
+handle_call(state, State) ->
+    {ok, {ok, State}, State};
+
+handle_call({expect, Expectation}, State) ->
+    {ok, NewState} = store_expectation(Expectation, State),
+    {ok, added, NewState};
+
+handle_call(assert_expectations, State) ->
+    Reply = handle_assert_expectations(State),
+    {ok, Reply, State};
+
+handle_call(Request, State) -> 
+    {ok, Reply, NewState} = reply_with_next_expectation(call, Request, State),
+    {ok, Reply, NewState}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%--------------------------------------------------------------------
+handle_info(Info, State) -> 
+    {ok, _Reply, NewState} = reply_with_next_expectation(info, Info, State),
+    {ok, NewState}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) -> 
+    ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) -> 
+    {ok, State}.
+
+%%
+%% private functions
+%%
+store_expectation(Expectation, State) -> % {ok, NewState}
+    NewExpectations = [Expectation|State#state.expectations],  
+    NewState = State#state{expectations = NewExpectations},
+    {ok, NewState}.
+
+pop(L) -> % {Result, NewList} | {undef, []}
+    case L of
+        [] -> {undef, []};
+        List -> {lists:last(List), lists:sublist(List, 1, length(List) - 1)}
+    end.
+
+pop_expectation(State) -> % {ok, Expectation, NewState}
+    {Expectation, RestExpectations} = case pop(State#state.expectations) of
+        {undef, []} -> ?raise(no_gen_event_mock_expectation);
+        {Head, Rest} -> {Head, Rest}
+    end,
+    NewState = State#state{expectations = RestExpectations},
+    {ok, Expectation, NewState}.
+
+handle_assert_expectations(State) -> % {ok, State}
+    ExpLeft = State#state.expectations,
+    case length(ExpLeft) > 0 of
+        true -> {unmet_gen_event_expectation, ExpLeft};
+        false -> 
+			case length(State#state.unexpected) of
+				0 ->
+					ok;
+				_Else ->
+					[H | _] = State#state.unexpected,
+					{unexpected_request_made, H}
+			end
+    end.
+
+reply_with_next_expectation(Type, Request, State) -> % -> {ok, Reply, NewState}
+    {ok, Expectation, NewState} = pop_expectation(State),
+	{ok, Reply, NewState2} = case Expectation#expectation.type of
+		 Type ->
+			try call_expectation_lambda(Expectation, Type, Request, NewState) of
+				{ok, R, State2} -> {ok, R, State2}
+			catch
+				error:function_clause ->
+					{ok, ok, NewState#state{unexpected = [{unexpected_request_made, Type, Request} | NewState#state.unexpected]}}
+			end;
+		_Else ->
+			{ok, ok, NewState#state{unexpected = [{unexpected_request_made, Type, Request} | NewState#state.unexpected]}}
+	end,
+	{ok, Reply, NewState2}.
+
+% hmm what if we want better response. 
+call_expectation_lambda(Expectation, Type, Request, State) -> % {ok, NewState}
+    L = Expectation#expectation.lambda,
+    Response = case Type of
+		call -> L(Request, State);
+		event -> L(Request, State);
+		info -> L(Request, State);
+		_ -> L(Request, State)
+	end,
+    case Response of % hmmm
+        ok                       -> {ok, ok,       State};
+        {ok, NewState}           -> {ok, ok,       NewState};
+        {ok, ResponseValue, NewState} -> {ok, ResponseValue, NewState};
+        Other -> Other
+    end.
---
+++ test/src/eunit_gen_event_mock.erl
@@ -0,0 +1,104 @@
+%%%-------------------------------------------------------------------
+%%% File          : eunit_gen_event_mock.erl
+%%% Author        : Micah Warren
+%%% Organization  : SpiceCSM
+%%% Project       : gen_server_mock
+%%% Description   : 
+%%%
+%%% Created       :  8/25/09
+%%%-------------------------------------------------------------------
+-module(eunit_gen_event_mock).
+-author(micahw).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define (DEBUG, true).
+-define (TRACE(X, M), case ?DEBUG of
+  true -> io:format(user, "TRACE ~p:~p ~p ~p~n", [?MODULE, ?LINE, X, M]);
+  false -> ok
+end).
+
+-define(exit_error_name(Exception),
+    ((fun () ->
+    {ErrorName, _Info } = Exception,
+    ErrorName
+    end)())).
+
+setup() ->
+    ok.
+
+teardown(_Arg) ->
+    ok.
+
+all_test_() ->
+	{setup,
+	fun setup/0,
+	fun teardown/1,
+	[{"everything's okay",
+	fun() ->
+		{ok, Mock} = gen_event_mock:start(mock1),
+		gen_event_mock:expect(Mock, call, fun({foo, hi}, _State) -> ok end),
+		gen_event_mock:expect_call(Mock, fun({bar, bye}, _State) -> ok end),
+		
+		ok = gen_event:call(Mock, gen_event_mock, {foo, hi}),
+		ok = gen_event:call(Mock, gen_event_mock, {bar, bye}),
+		
+		ok = gen_event_mock:assert_expectations(Mock),
+		
+		gen_event_mock:stop(Mock),
+		
+		{ok}
+	end},
+	{"missing expectations",
+	fun() ->
+		{ok, Mock} = gen_event_mock:start(mock2),
+		gen_event_mock:expect(Mock, call, fun({foo, hi}, _State) -> ok end),
+		gen_event_mock:expect_call(Mock, fun({bar, bye}, _State) -> ok end),
+		
+		ok = gen_event:call(Mock, gen_event_mock, {foo, hi}),
+		
+		?assertError({unmet_gen_event_expectation, _}, gen_event_mock:assert_expectations(Mock)),
+%		Result = try gen_event_mock:assert_expectations(Mock)
+%		catch
+%			exit:Exceptions -> Exceptions
+%		end,
+%		?debugFmt("Result:  ~p", [Result]),
+%		ErrorName = ?exit_error_name(Result),
+%		?assertEqual(unmet_gen_leader_expectation, ErrorName),
+		
+		{ok}
+	end},
+	{"unexpected_messages",
+	fun() ->
+		{ok, Mock} = gen_event_mock:start(mock3),
+		gen_event_mock:expect_call(Mock, fun({bar, bye}, _State) -> ok end),
+		gen_event:call(Mock, gen_event_mock, {foo, hi}),
+
+		?assertError({unexpected_request_made, _}, gen_event_mock:assert_expectations(Mock)),
+		
+		{ok}
+	end},
+	{"special return values",
+	fun() ->
+		{ok, Mock} = gen_event_mock:start(mock4),
+		
+		gen_event_mock:expect_call(Mock, fun(call_one, _State) -> ok end),
+		gen_event_mock:expect_call(Mock, fun(call_two, State) -> {ok, State} end),
+		gen_event_mock:expect_call(Mock, fun(call_three, State) -> {ok, good, State} end),
+		gen_event_mock:expect_event(Mock, fun(event_one, _State) -> ok end),
+		gen_event_mock:expect_event(Mock, fun(event_two, _State) -> ok end),
+		gen_event_mock:expect_info(Mock, fun(info_one, _State) -> ok end),
+		
+		?assertEqual(ok, gen_event:call(Mock, gen_event_mock, call_one)),
+		?assertEqual(ok, gen_event:call(Mock, gen_event_mock, call_two)),
+		?assertEqual(good, gen_event:call(Mock, gen_event_mock, call_three)),
+		?assertEqual(ok, gen_event:notify(Mock, event_one)),
+		?assertEqual(ok, gen_event:sync_notify(Mock, event_two)),
+		Mock ! info_one,
+		
+		?assertEqual(ok, gen_event_mock:assert_expectations(Mock)),
+		
+		gen_event_mock:stop(Mock),
+		
+		{ok}
+	end}]}.
